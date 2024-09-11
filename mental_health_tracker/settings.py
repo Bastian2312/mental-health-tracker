74b16a72cc88932a37a2fe6c7a1e584f66b4e937
Bastian2312 <bastian.adiputras@gmail.com>
2024-09-11 16:08:34 +0700

    Push to PWS automatic

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-((o$uw%(c6-8q1hc)bm$40%qa4%*29xda(f9l)y@_ot$-5k)&4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bastian-adiputra-mentalhealthtracker.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bastian-adiputra-mentalhealthtracker1.pbp.cs.ui.ac.id"]
 
 
 # Application definition
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://<URL PWS KAMU>", "https://bastian-adiputra-mentalhealthtracker1.pbp.cs.ui.ac.id"]
