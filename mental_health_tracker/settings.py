5306745a896f024cfd730847f4adca126a0e6d6f
Bastian2312 <bastian.adiputras@gmail.com>
2024-09-13 13:16:11 +0700

    mentalhealthtracker1 --> mentalhealthtracker

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://<URL PWS KAMU>", "https://bastian-adiputra-mentalhealthtracker1.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://<URL PWS KAMU>", "https://bastian-adiputra-mentalhealthtracker.pbp.cs.ui.ac.id"]
